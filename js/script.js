aa5d12e5f5494ca5797c255bf516ec26f1f83ae4
mohatamimhaque <72484860+mohatamimhaque@users.noreply.github.com>
2022-01-12 23:25:05 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+var icon = document.querySelector(".language-icon");
+var item = document.querySelector(".language-item");
+
+icon.onclick = function(){
+   item.classList.toggle("language-list")
+}
